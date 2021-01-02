6f15e5be6e9fc52651b5db2c2909d4eef2f86fab
Nguyễn Mạnh Cường <nguyenmanhcuong.stf@gmail.com>
2021-01-02 12:13:39 +0700

    Updates

---
+++ precache-manifest.59e22f3d93f54073e6809650e5c8d35f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52858d273f9b5f67eed19dc956afb0f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dd093d9fbe9ace89afad",
+    "url": "./static/css/main.6756054b.chunk.css"
+  },
+  {
+    "revision": "4a344bd9785fa7361294",
+    "url": "./static/js/2.b5763cfc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b5763cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd093d9fbe9ace89afad",
+    "url": "./static/js/main.4158809b.chunk.js"
+  },
+  {
+    "revision": "677d8979f96e17bb3de1",
+    "url": "./static/js/runtime-main.db31b942.js"
+  }
+]);
